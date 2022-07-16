f5ae6646f20b0ce753f4720752f248460093d9ad
Talia <taliabennaim@gmail.com>
2022-07-15 23:12:36 -0400

    restructured code for training model, making predictions, and evaluating model that was formerly under main\nseperated into a function for if a SearchCV workflow is used or a new function for if a train/test data split workflow is used

--- avo.py
+++ avo.py
@@ -11,6 +11,8 @@ from sklearn.pipeline import Pipeline
 # from sklearn.preprocessing import OneHotEncoder
 import matplotlib.pylab as plt
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, mean_absolute_error
 from sklego.meta import GroupedPredictor #WARNING: check how balanced groups are bfore use
 import pandas as pd
 
@@ -38,12 +40,29 @@ def load_data(file_path: str):
 	# add test that post processing all data in x is numeric
 	return X, y
 
-def evaluate_model(mod: GridSearchCV):
-	pred = mod.fit(X, y).predict(X)
+
+def evaluate_model(pred,y):
 	plt.scatter(pred, y)
 	plt.show()
+	print(f"accuracy: {str(mean_absolute_error(y, pred))}")
+
+
+def grid_search(pipe, X, y):
+	mod = GridSearchCV(estimator=pipe,
+                 param_grid={
+                   'model__n_neighbors': list(range(10,20))
+                 },
+                 cv=3) 
+	mod.fit(X,y)
 	pd.DataFrame(mod.cv_results_).to_excel("grid_search_results.xlsx")
-	return("my metric of choice")
+	evaluate_model(mod.predict(X),y)
+
+
+def train_test(pipe, X, y):
+	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+	pipe.fit(X_train, y_train)
+	evaluate_model(pipe.predict(X_test),y_test)
+	
 
 if __name__ == "__main__":
 	X, y = load_data("avocado-data.csv")
@@ -53,15 +72,10 @@ if __name__ == "__main__":
 	# model = LogisticRegression()
 	# model = LinearRegression() # linear regression did poorly
 	model = KNeighborsRegressor(n_neighbors=10)
-	enc = OneHotEncoder(handle_unknown="ignore")
 	pipe = Pipeline([
 		("scale", scaler),
 		("model", model)
 	])
 
-	mod = GridSearchCV(estimator=pipe,
-                 param_grid={
-                   'model__n_neighbors': list(range(10,20))
-                 },
-                 cv=2) 
-	evaluate_model(mod)
+	pred=train_test(pipe,X,y)
+	pred=grid_search(pipe,X,y)
