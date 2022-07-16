326e250e2294e9e5c65295d71f1101691bb25945
Talia <taliabennaim@gmail.com>
2022-07-15 21:43:00 -0400

    added 'evaluate_model' function

--- avo.py
+++ avo.py
@@ -2,6 +2,7 @@
 # https://www.youtube.com/watch?v=0B5eIE_1vpU 54:00
 # imports
 import matplotlib.pylab as plt
+from sklearn.preprocessing import StandardScaler, QuantileTransformer
 from sklearn.linear_model import LogisticRegression
 # from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
@@ -10,6 +11,7 @@ from sklearn.pipeline import Pipeline
 # from sklearn.preprocessing import OneHotEncoder
 import matplotlib.pylab as plt
 from sklearn.model_selection import GridSearchCV
+from sklego.meta import GroupedPredictor #WARNING: check how balanced groups are bfore use
 import pandas as pd
 
 
@@ -26,14 +28,23 @@ def load_data(file_path: str):
 	df = pd.read_csv(file_path, parse_dates=["date"])
 	df["date"] = df["date"].dt.month # convert dates into numerical representation of months i.e. 1:12
 	# TODO: do date by cyclic function like sin
+	# TODO: check how year is scaled: should it just be years since start instead?
 	# TODO: https://stackoverflow.com/questions/60153981/scikit-learn-one-hot-encoding-certain-columns-of-a-pandas-dataframe
 	# X = df.loc[:, df.columns!="average_price"]
 	X = df.loc[:, df.columns!="average_price"].iloc[:,:9]
 	y = df["average_price"]
+	# add test that post processing all data in x is numeric
 	return X, y
 
+def evaluate_model(mod: GridSearchCV):
+	pred = mod.fit(X, y).predict(X)
+	plt.scatter(pred, y)
+	plt.show()
+	pd.DataFrame(mod.cv_results_).to_excel("grid_search_results.xlsx")
+	return("my metric of choice")
 
 if __name__ == "__main__":
+	X, y = load_data("avocado-data.csv")
 	# scaler = StandardScaler() # Quantile was slightly better
 	scaler = QuantileTransformer(n_quantiles=100)
 	# scaler = PolynomialFeatures() # bad
@@ -41,19 +52,14 @@ if __name__ == "__main__":
 	# model = LinearRegression() # linear regression did poorly
 	model = KNeighborsRegressor(n_neighbors=10)
 	enc = OneHotEncoder(handle_unknown="ignore")
-	X, y = load_data("avocado-data.csv")
 	pipe = Pipeline([
 		("scale", scaler),
 		("model", model)
 	])
-	pred = pipe.fit(X, y).predict(X)
-	plt.scatter(pred, y)
-	plt.show()
-	# mod = GridSearchCV(estimator=pipe,
-    #              param_grid={
-    #                'model__n_neighbors': list(range(10,20))
-    #              },
-    #              cv=3) 
-	# mod.fit(X, y)
-	
-	# pd.DataFrame(mod.cv_results_).to_clipboard
+
+	mod = GridSearchCV(estimator=pipe,
+                 param_grid={
+                   'model__n_neighbors': list(range(10,20))
+                 },
+                 cv=2) 
+	evaluate_model(mod)
